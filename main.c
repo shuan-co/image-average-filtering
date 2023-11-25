0c97e2df85bc457ab79a2798737d676e36cf5890
Shuan Co <78131504+shuan-co@users.noreply.github.com>
2023-11-25 17:28:10 +0800

    DONE

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 
 extern void add(int *, int *, int, int, int);
 
 int main()
-{
+{   
+    //  PARAMETERS
     int arr[] = {1, 4, 0, 1, 3, 1,
                  2, 2, 4, 2, 2, 3,
                  1, 0, 1, 0, 1, 0,
@@ -12,21 +14,17 @@ int main()
                  2, 5, 3, 1, 2, 5,
                  1, 1, 4, 2, 3, 0};
 
-    int arr2[] = {1, 4, 0, 1, 3, 1,
-                  2, 2, 4, 2, 2, 3,
-                  1, 0, 1, 0, 1, 0,
-                  1, 2, 1, 0, 2, 2,
-                  2, 5, 3, 1, 2, 5,
-                  1, 1, 4, 2, 3, 0};
+    int arr2[sizeof(arr)/4]; 
+    memcpy(arr2, arr, sizeof(arr));
+
+    //  PARAMETERS
     int image_size_x = 6;
     int image_size_y = 6;
-    int sampling_window_size = 3;
+    int sampling_window_size = 5;
 
+    // DO NOT TOUCH
     int n = sizeof(arr) / sizeof(arr[0]);
     add(arr, arr2, image_size_x, image_size_y, sampling_window_size);
-
-        printf("Done\n");
-
 
     printf("Contents of arr1:\n");
     for(int i = 0; i < n; i++) {
